c120e64e8790bcddc518dd049256856bbaab157c
LatorreSony <latorretorras@gmail.com>
2025-12-28 20:46:37 +0100

    Revert "Added activation code"
    This reverts commit c87d18e4d739a49092325a7f646c8cccb432c9a4.

--- src/routes/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ cookies }) => {
-	const accessGranted = cookies.get('access_granted');
-	return {
-		hasAccess: accessGranted === 'true'
-	};
-};
